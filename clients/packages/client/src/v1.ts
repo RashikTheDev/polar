a5e263674ab0a21054d5277060beb4eab0f2706c
Pieter Beulque <pieterbeulque@gmail.com>
2026-04-08 20:35:07 +0200

    Bring back email into the subscription notification

--- clients/packages/client/src/v1.ts
+++ clients/packages/client/src/v1.ts
@@ -19423,6 +19423,8 @@ export interface components {
     MaintainerNewPaidSubscriptionNotificationPayload: {
       /** Subscriber Name */
       subscriber_name: string
+      /** Subscriber Email */
+      subscriber_email?: string | null
       /** Tier Name */
       tier_name: string
       /** Tier Price Amount */
